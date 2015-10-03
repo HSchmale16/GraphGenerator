d59b33d02330d9566947f86dea26bbbe435a0e38
Henry J Schmale <hschmale16@gmail.com>
2015-10-02 20:23:37 -0400

    More Stuff
    * JSON is loaded
    * Parse flags are now handled
    * Rename file so that it is clear what it is testing

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@
  *      ./graphgen <datafile> <img_format> <outfile>
  */
 
-#include <string>
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <streambuf>
 #include <json.hpp>
 #include <CImg.h>
 
@@ -16,13 +18,27 @@ namespace cimg = cimg_library;
 namespace json = nlohmann;
 
 int main(int argc, char** argv){
-    // Parse Command Line Args
+    // Parse Command Line Args and do init
     cimg_usage("Generates a graph from json file");
-    const char* infile = cimg_option("-i", "graph.json",
+    const char* infile_name = cimg_option("-i", "graph.json",
             "Graph Definition File");
-    const char* outfile = cimg_option("-o", (char*)NULL,
+    const char* outfile_name = cimg_option("-o", (char*)NULL,
             "The image file to put the generated graph into");
 
-    // begin parse
+    if(!outfile_name){
+        std::cerr << "Outfile must be specified" << std::endl;
+        return 0;
+    }
 
+    std::ifstream infile(infile_name);
+    if((!infile_name) or (!infile)){
+        std::cerr << "Infile could not be opened" << std::endl;
+        return 0;
+    }
+    std::string indata((std::istreambuf_iterator<char>(infile)),
+                        std::istreambuf_iterator<char>());
+
+    json::json j_complete = json::json::parse(indata);
+
+    infile.close();
 }
